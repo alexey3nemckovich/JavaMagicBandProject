15fa7b8f9b3344780ab3dd448b49896591fc2038
Nuskit <nuskit@mail.com>
2017-02-27 14:36:49 +0300

    Extend binder options.

--- src/main/com/bsuir/autoservice/binder/impl/DefaultBinder.java
+++ src/main/com/bsuir/autoservice/binder/impl/DefaultBinder.java
@@ -1,5 +1,7 @@
 package main.com.bsuir.autoservice.binder.impl;
 
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+import javafx.util.Pair;
 import main.com.bsuir.autoservice.binder.IBinder;
 import main.com.bsuir.autoservice.binder.exception.BinderException;
 
@@ -8,25 +10,53 @@ import java.lang.reflect.Method;
 import java.util.Map;
 
 public class DefaultBinder implements IBinder {
-    private Object generateNewObject(Class<?> aClass) throws IllegalAccessException, InstantiationException {
+    private static final String methodName = "valueOf";
+
+    private Object generateNewObject(Class aClass) throws IllegalAccessException, InstantiationException {
         return aClass.newInstance();
     }
 
-    private Object getMappedObject(String[] checkedParameters, Map<String, String[]> parameters, Class returnType) throws InstantiationException, IllegalAccessException {
+    private Object getMappedObject(Class returnType, Map<String, String[]> parameters, String[] checkedParameters)
+            throws InstantiationException, IllegalAccessException {
         Object newObject = generateNewObject(returnType);
         for (String checkedParameter: checkedParameters){
             if (parameters.containsKey(checkedParameter)) {
                 updateObject(newObject, checkedParameter, parameters.get(checkedParameter));
             }else{
-                updateDefaultObject(newObject, checkedParameter);
+                //TODO: log unmapped Object
             }
         }
         return newObject;
     }
 
-    private void updateDefaultObject(Object newObject, String parameter){
+    private Object getMappedObject(Class returnType, Map<String, String[]> parameters)
+            throws InstantiationException, IllegalAccessException {
+        Object newObject = generateNewObject(returnType);
+        for (Field fieldDeclaration: returnType.getFields()){
+            if (parameters.containsKey(fieldDeclaration.getName())) {
+                updateObject(newObject, fieldDeclaration, parameters.get(fieldDeclaration.getName()));
+            }else{
+                updateDefaultObject(newObject, fieldDeclaration);
+            }
+        }
+        return newObject;
+    }
+
+    private Object getMappedObject(Class returnType, Map<String, String[]> parameters, Pair<String,String>[] checkedMapParameters)
+            throws InstantiationException, IllegalAccessException {
+        Object newObject = generateNewObject(returnType);
+        for (Pair<String,String> checkedMapParameter: checkedMapParameters){
+            if (parameters.containsKey(checkedMapParameter.getKey())) {
+                updateObject(newObject, checkedMapParameter.getValue(), parameters.get(checkedMapParameter.getKey()));
+            }else{
+                //TODO: log unmapped Object
+            }
+        }
+        return newObject;
+    }
+
+    private void updateDefaultObject(Object newObject, Field fieldDeclaration){
         try {
-            Field fieldDeclaration = getFieldDeclaration(newObject,parameter);
             fieldDeclaration.set(newObject, fieldDeclaration.getType().newInstance());
         }catch (Exception e) {
             //TODO: log that invalid parameters
@@ -34,14 +64,13 @@ public class DefaultBinder implements IBinder {
     }
 
     private Field getFieldDeclaration(Object newObject, String parameter) throws NoSuchFieldException {
-        Class<?> objectClass = newObject.getClass();
+        Class objectClass = newObject.getClass();
         return objectClass.getField(parameter);
     }
 
-    private void updateObject(Object newObject, String parameter, String[] currentValues) {
+    private void updateObject(Object newObject, Field fieldDeclaration, String[] currentValues) {
         try {
-            Field fieldDeclaration = getFieldDeclaration(newObject,parameter);
-            Method convertMethod = fieldDeclaration.getType().getMethod("valueOf", String.class);
+            Method convertMethod = fieldDeclaration.getType().getMethod(methodName, String.class);
             //TODO: think can more 1 arguments
             fieldDeclaration.set(newObject, convertMethod.invoke(null, currentValues[0]));
         }catch (Exception e) {
@@ -49,10 +78,39 @@ public class DefaultBinder implements IBinder {
         }
     }
 
-    @Override
-    public Object mappedParameters(Class returnType, String[] checkedParameters, Map<String, String[]> parameters) throws BinderException {
+    private void updateObject(Object newObject, String parameter, String[] currentValues) {
         try {
-            return getMappedObject(checkedParameters,parameters,returnType);
+            Field fieldDeclaration = getFieldDeclaration(newObject,parameter);
+            updateObject(newObject,fieldDeclaration,currentValues);
+        }catch (Exception e) {
+            //TODO: log that invalid parameters
+        }
+    }
+
+    @Override
+    public Object mappedParameters(Class returnType, Map<String, String[]> parameters, String[] checkedParameters)
+            throws BinderException {
+        try {
+            return getMappedObject(returnType,parameters,checkedParameters);
+        }catch (Exception e){
+            throw new BinderException(e);
+        }
+    }
+
+    @Override
+    public Object mappedParameters(Class returnType, Map<String, String[]> parameters) throws BinderException {
+        try {
+            return getMappedObject(returnType,parameters);
+        }catch (Exception e){
+            throw new BinderException(e);
+        }
+    }
+
+    @Override
+    public Object mappedParameters(Class returnType, Map<String, String[]> parameters, Pair<String, String>[] checkedMapParameters)
+            throws BinderException {
+        try {
+            return getMappedObject(returnType,parameters,checkedMapParameters);
         }catch (Exception e){
             throw new BinderException(e);
         }
