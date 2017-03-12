e839923065b937c4e239ac7e479397abbec53e16
Nuskit <nuskit@mail.com>
2017-03-12 15:48:56 +0300

    Rename Binder to Mapper. Mapper haves BindingFactory. Works jsp notations. Add log4j.

--- src/main/com/bsuir/autoservice/command/impl/UserCommand.java
+++ src/main/com/bsuir/autoservice/command/impl/UserCommand.java
@@ -1,6 +1,6 @@
 package main.com.bsuir.autoservice.command.impl;
 
-import main.com.bsuir.autoservice.library.binder.IBinder;
+import main.com.bsuir.autoservice.library.mapper.IMapper;
 import main.com.bsuir.autoservice.command.ICommand;
 import main.com.bsuir.autoservice.command.exception.CommandException;
 import main.com.bsuir.autoservice.dto.UserDTO;
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UserCommand implements ICommand {
-    public UserCommand(IBinder binder){
+    public UserCommand(IMapper binder){
         this.binder = binder;
     }
 
@@ -18,12 +18,12 @@ public class UserCommand implements ICommand {
         try {
             UserDTO userDTO = (UserDTO) binder.mappedParameters(UserDTO.class, request.getParameterMap());
             request.setAttribute("data",userDTO);
-            request.setAttribute("ids",userDTO.id);
+            request.setAttribute("ids",userDTO.name.toString());
             request.getRequestDispatcher("user.jsp").forward(request, response);
         }catch (Exception e){
             throw new CommandException(e);
         }
     }
 
-    private final IBinder binder;
+    private final IMapper binder;
 }
